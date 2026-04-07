822e2fb2c9a0738078b0355ba83149cad9bfea2a
Yuseless <nsiclementpons@gmail.com>
2026-04-07 22:53:05 +0200

    1ère partie

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -1,10 +1,15 @@
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using Unity.VisualScripting;
+using UnityEngine;
 
 public static class Timer
 {
     private static readonly Stopwatch stopwatch = new();
     private static List<long> steps = new();
+    private static string filePath = "score.txt";
 
     public static bool IsRunning
     {
@@ -53,12 +58,60 @@ public static class Timer
     public static void Save()
     {
         // TODO : save our time steps (line 7 of this script) inside a file.
+
+        long TempActuel = stopwatch.ElapsedMilliseconds;
+
+        if (File.Exists(filePath))
+        {
+            string Coder = File.ReadAllText(filePath);
+            string Decoder = System.Text.Encoding.UTF8.GetString(System.Convert.FromBase64String(Coder));
+            string[] Lines = Decoder.Split('\n');
+
+            if (Lines.Length > 0 && long.TryParse(Lines[Lines.Length - 1], out long bestTime))
+            {
+                if (TempActuel < bestTime)
+                {
+                    UnityEngine.Debug.Log("Nouveau record! " + TempActuel + "ms");
+                }
+                else
+                {
+                    UnityEngine.Debug.Log("Temps Actuel: " + TempActuel + "ms, Meilleur temps: " + bestTime + "ms");
+                    return;
+                }
+            }
+        }
+        List<string> linesToSave = new List<string>();
+        foreach (long step in steps)
+        {
+            linesToSave.Add(step.ToString());
+        }
+
+        string joined = string.Join("\n", linesToSave);
+        string encodedData = System.Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes(joined));
+
+        File.WriteAllText(filePath, encodedData);
     }
 
     public static void Load()
     {
-        // TODO : load our time steps from a file (if we have any)
-        // and store them inside our steps variable (line 7 of this script)
-        // to show them to the player before starting a race.
+        if (File.Exists(filePath))
+        {
+            // TODO : load our time steps from a file (if we have any)
+            // and store them inside our steps variable (line 7 of this script)
+            // to show them to the player before starting a race.
+            string Coder = File.ReadAllText(filePath);
+            string Decoder = System.Text.Encoding.UTF8.GetString(System.Convert.FromBase64String(Coder));
+            string[] Lines = Decoder.Split('\n');
+
+            steps.Clear();
+
+            foreach (string Line in Lines)
+            {
+                if (long.TryParse(Line, out long value))
+                {
+                    steps.Add(value);
+                }
+            }
+        }
     }
 }
